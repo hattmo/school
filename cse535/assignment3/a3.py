42a412ec49a5152e83c78ce6d4ba6309e122b457
matthew <matthew@hattmo.com>
2025-09-30 00:05:38 -0400

    a3 done

--- cse535/assignment3/a3.py
+++ cse535/assignment3/a3.py
@@ -49,13 +49,13 @@ class A3:
     def generate_power_statistics(self, weights):
         if self.df is None:
             raise Exception()
-        energy = (
+        weighted_sum = (
             self.df['CPU_Usage_Percent'] * weights[0] +
             self.df['Memory_Usage_MB'] * weights[1] +
             self.df['Network_Activity_KB'] * weights[2] +
             self.df['Disk_IO_MB'] * weights[3]
-        ) * self.df['Execution_Time_s']
-        power = energy / self.df['Execution_Time_s']
+        )
+        power = weighted_sum / self.df['Execution_Time_s']
         power = np.round(power, 2)
         avg = np.round(np.mean(power), 2)
         max_app = self.df['Application'][np.argmax(power)]
@@ -65,26 +65,15 @@ class A3:
     def calculate_power_sensitivites(self, weights):
         if self.df is None:
             raise Exception()
-        cpu = np.sum(self.df['CPU_Usage_Percent'] * weights[0])
-        mem = np.sum(self.df['Memory_Usage_MB'] * weights[1])
-        net = np.sum(self.df['Network_Activity_KB'] * weights[2])
-        disk = np.sum(self.df['Disk_IO_MB'] * weights[3])
-
+        cpu = np.sum(self.df['CPU_Usage_Percent'] * weights[0] / self.df['Execution_Time_s'])
+        mem = np.sum(self.df['Memory_Usage_MB'] * weights[1] / self.df['Execution_Time_s'])
+        net = np.sum(self.df['Network_Activity_KB'] * weights[2] / self.df['Execution_Time_s'])
+        disk = np.sum(self.df['Disk_IO_MB'] * weights[3] / self.df['Execution_Time_s'])
         total = cpu + mem + net + disk
-
         sensitivities = [
             np.round(cpu / total * 100, 2),
             np.round(mem / total * 100, 2),
             np.round(net / total * 100, 2),
             np.round(disk / total * 100, 2)
         ]
-        return tuple(sensitivities)
-
-
-def main():
-    a = A3()
-    a.read_dataset()
-    a.generate_energy_statistics([1,2,3,4])
-
-if __name__ == '__main__':
-    main()
+        return tuple(np.float64(x) for x in sensitivities)
